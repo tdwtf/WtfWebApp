bd3e394bf073b71d4834deda2e7de2a0bed39cc5
kharnagy <kharnagy@inedo.com>
2014-05-30 19:43:12 -0400

    Added version number

--- TheDailyWtf/ViewModels/WtfViewModelBase.cs
+++ TheDailyWtf/ViewModels/WtfViewModelBase.cs
@@ -1,10 +1,18 @@
 ﻿using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
 using TheDailyWtf.Models;
 
 namespace TheDailyWtf.ViewModels
 {
     public abstract class WtfViewModelBase
     {
+        private static readonly string copyright = typeof(WtfViewModelBase).Assembly.GetCustomAttributes(typeof(AssemblyCopyrightAttribute), false)
+                .Cast<AssemblyCopyrightAttribute>()
+                .First()
+                .Copyright;
+        private static readonly string version = typeof(WtfViewModelBase).Assembly.GetName().Version.ToString(2);
+
         public WtfViewModelBase()
         {
             this.PageTitle = "[UNITITLED] The Daily WTF: Curious Perversions in Information Technology";
@@ -13,5 +21,7 @@ namespace TheDailyWtf.ViewModels
         public string PageTitle { get; set; }
         public IEnumerable<ArticleModel> RecentArticles { get { return ArticleModel.GetRecentArticles(); } }
         public NavigationMenuViewModel NavigationMenu { get { return new NavigationMenuViewModel(); } }
+        public string Copyright { get { return copyright; } }
+        public string Version { get { return version; } }
     }
 }
