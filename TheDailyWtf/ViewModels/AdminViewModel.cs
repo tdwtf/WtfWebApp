82dd9b0d249892288099c2834ce63b40307169d6
PaulaBean <paula@thedailywtf.com>
2014-10-17 18:18:24 -0400

    Cached article fixes

--- TheDailyWtf/ViewModels/AdminViewModel.cs
+++ TheDailyWtf/ViewModels/AdminViewModel.cs
@@ -5,6 +5,7 @@ namespace TheDailyWtf.ViewModels
 {
     public class AdminViewModel : WtfViewModelBase
     {
+        public IEnumerable<ArticleModel> PublishedArticles { get { return ArticleModel.GetRecentArticles(25); } }
         public IEnumerable<ArticleModel> UnpublishedArticles { get { return ArticleModel.GetUnpublishedArticles(); } }
         public IEnumerable<SeriesModel> AllSeries { get { return SeriesModel.GetAllSeries(); } }
         public IEnumerable<AuthorModel> AllAuthors { get { return AuthorModel.GetAllAuthors(); } }
