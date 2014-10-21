440eedc8963fc191ec1f3d22ec67ae913513eb9a
PaulaBean <paula@thedailywtf.com>
2014-10-21 14:27:08 -0400

    Fix #74

--- TheDailyWtf/ViewModels/ViewCommentsViewModel.cs
+++ TheDailyWtf/ViewModels/ViewCommentsViewModel.cs
@@ -11,17 +11,16 @@ namespace TheDailyWtf.ViewModels
         {
             this.Article = article;
             this.Comments = CommentModel.FromArticle(article);
-            this.MaxDiscoursePostId = this.Comments.Any() ? this.Comments.Max(c => c.DiscoursePostId ?? 0) : 0;
-            if (this.MaxDiscoursePostId > 0)
-                this.DiscourseTopicUrl = this.Article.DiscourseThreadUrl + "/" + this.MaxDiscoursePostId;
+            if (this.Article.CachedCommentCount > 1)
+                this.DiscourseNextUnreadCommentUrl = this.Article.DiscourseThreadUrl + "/" + this.Article.CachedCommentCount;
             else
-                this.DiscourseTopicUrl = this.Article.DiscourseThreadUrl;
+                this.DiscourseNextUnreadCommentUrl = this.Article.DiscourseThreadUrl;
         }
 
         public ArticleModel Article { get; private set; }
         public IEnumerable<CommentModel> Comments { get; private set; }
         public int MaxDiscoursePostId { get; private set; }
-        public string DiscourseTopicUrl { get; private set; }
+        public string DiscourseNextUnreadCommentUrl { get; private set; }
         public string ViewCommentsHeading 
         { 
             get 
