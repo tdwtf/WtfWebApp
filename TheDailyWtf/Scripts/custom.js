29097dc2e497cbea9903ea730be0df6c734001cb
bryantorosian <bryan.torosian@gmail.com>
2014-10-21 22:05:40 -0400

    Update custom.js
    Adding function to get Cookie Value

--- TheDailyWtf/Scripts/custom.js
+++ TheDailyWtf/Scripts/custom.js
@@ -7,3 +7,15 @@ $(document).ready(function () {
     });
 
 });
+
+//http://www.w3schools.com/js/js_cookies.asp
+function getCookie(cname) {
+    var name = cname + "=";
+    var ca = document.cookie.split(';');
+    for(var i=0; i<ca.length; i++) {
+        var c = ca[i];
+        while (c.charAt(0)==' ') c = c.substring(1);
+        if (c.indexOf(name) != -1) return c.substring(name.length,c.length);
+    }
+    return "";
+}
