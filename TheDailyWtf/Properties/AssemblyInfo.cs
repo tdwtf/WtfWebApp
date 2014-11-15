4cddf8325147e0ecb4016eb4a471ef379b29cecf
Alex Papadimoulis <apapadimoulis@inedo.com>
2014-11-15 11:39:29 -0500

    Update AssemblyInfo.cs

--- TheDailyWtf/Properties/AssemblyInfo.cs
+++ TheDailyWtf/Properties/AssemblyInfo.cs
@@ -4,7 +4,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyTitle("TheDailyWtf")]
 [assembly: AssemblyDescription("The website for TheDailyWtf.com")]
 [assembly: AssemblyProduct("TheDailyWtf")]
-[assembly: AssemblyCopyright("Copyright © 2004 - 2014 Alex Papadimoulis")]
+[assembly: AssemblyCopyright("Copyright © 2004 - 2014 Inedo Publishing")]
 [assembly: ComVisible(false)]
 [assembly: Guid("5ed1f161-6ccb-4377-a390-c18ad123d473")]
 [assembly: AssemblyVersion("1.0.0.0")]
