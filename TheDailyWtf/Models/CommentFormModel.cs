a4512f6c24964702f694857cfa12efa38f8fe4ec
Ben Lubar <ben.lubar@gmail.com>
2016-02-01 09:26:39 -0600

    allow HTML-like tags in comments and commenter names
    prevent HTML from rendering as the first part of a paragraph in markdown

--- TheDailyWtf/Models/CommentFormModel.cs
+++ TheDailyWtf/Models/CommentFormModel.cs
@@ -1,10 +1,13 @@
-﻿
+﻿using System.Web.Mvc;
+
 namespace TheDailyWtf.Models
 {
     public class CommentFormModel
     {
         public int? Parent { get; set; }
+        [AllowHtml]
         public string Name { get; set; }
+        [AllowHtml]
         public string Body { get; set; }
     }
 }
--- TheDailyWtf/Models/CommentModel.cs
+++ TheDailyWtf/Models/CommentModel.cs
@@ -114,6 +114,19 @@ namespace TheDailyWtf.Models
             {
             }
 
+            protected override void WriteBlock(Block block, bool isOpening, bool isClosing, out bool ignoreChildNodes)
+            {
+                // prevent HTML from being rendered
+                if (block.Tag == BlockTag.HtmlBlock)
+                {
+                    ignoreChildNodes = true;
+                    WriteEncodedHtml(block.StringContent);
+                    return;
+                }
+
+                base.WriteBlock(block, isOpening, isClosing, out ignoreChildNodes);
+            }
+
             protected override void WriteInline(Inline inline, bool isOpening, bool isClosing, out bool ignoreChildNodes)
             {
                 // prevent HTML from being rendered
