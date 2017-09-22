26ccc99275dc69589841d9f9e5490b94f0228e6b
Ben Lubar <ben.lubar@gmail.com>
2017-09-22 10:36:14 -0500

    Add article title to non-article comment admin views

--- TheDailyWtf/ViewModels/ViewCommentsViewModel.cs
+++ TheDailyWtf/ViewModels/ViewCommentsViewModel.cs
@@ -15,6 +15,7 @@ namespace TheDailyWtf.ViewModels
             var comments = CommentModel.FromArticle(article);
             this.AllComments = comments;
             this.Comments = comments.Skip((page - 1) * CommentsPerPage).Take(CommentsPerPage).ToList();
+            this.Articles = new Dictionary<int, ArticleModel> { { article.Id.Value, article } };
             this.PageNumber = page;
 
             this.Comment = new CommentFormModel();
@@ -24,6 +25,7 @@ namespace TheDailyWtf.ViewModels
         {
             this.Article = article;
             this.Comments = comments;
+            this.Articles = new Dictionary<int, ArticleModel> { { article.Id.Value, article } };
             this.PageNumber = -1;
         }
 
@@ -32,6 +34,7 @@ namespace TheDailyWtf.ViewModels
             this.Article = null;
             this.AllComments = comments;
             this.Comments = comments.Skip((page - 1) * CommentsPerPage).Take(CommentsPerPage).ToList();
+            this.Articles = this.Comments.Select(c => c.ArticleId).Distinct().ToDictionary(id => id, id => ArticleModel.GetArticleById(id));
             this.PageNumber = page;
         }
 
@@ -40,6 +43,7 @@ namespace TheDailyWtf.ViewModels
         public virtual bool CanEditDelete { get { return false; } }
         public virtual bool CanReply { get { return this.PageNumber != -1; } }
         public ArticleModel Article { get; private set; }
+        public IReadOnlyDictionary<int, ArticleModel> Articles { get; private set; }
         public IList<CommentModel> AllComments { get; protected set; }
         public IList<CommentModel> Comments { get; protected set; }
         public int PageNumber { get; private set; }
